29ef07bdc5ddc3e2622f626f2caad0de69053469
a6a18 <39345520+a6a18@users.noreply.github.com>
2022-12-20 17:35:11 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,58 @@
 import React from 'react';
-import { Layout } from 'antd';
-
-const { Header, Footer, Sider, Content } = Layout;
-
-const App: React.FC = () => (
-    <>
-        <Layout>
-            <Header>Header</Header>
-            <Content>Content</Content>
-            <Footer>Footer</Footer>
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+const { Header, Content, Footer } = Layout;
+const App = () => {
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
+    return (
+        <Layout className="layout">
+            <Header>
+                <div className="logo" />
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={['2']}
+                    items={new Array(15).fill(null).map((_, index) => {
+                        const key = index + 1;
+                        return {
+                            key,
+                            label: `nav ${key}`,
+                        };
+                    })}
+                />
+            </Header>
+            <Content
+                style={{
+                    padding: '0 50px',
+                }}
+            >
+                <Breadcrumb
+                    style={{
+                        margin: '16px 0',
+                    }}
+                >
+                    <Breadcrumb.Item>Home</Breadcrumb.Item>
+                    <Breadcrumb.Item>List</Breadcrumb.Item>
+                    <Breadcrumb.Item>App</Breadcrumb.Item>
+                </Breadcrumb>
+                <div
+                    className="site-layout-content"
+                    style={{
+                        background: colorBgContainer,
+                    }}
+                >
+                    Content
+                </div>
+            </Content>
+            <Footer
+                style={{
+                    textAlign: 'center',
+                }}
+            >
+                Ant Design ©2018 Created by Ant UED
+            </Footer>
         </Layout>
-
-        <Layout>
-            <Header>Header</Header>
-            <Layout>
-                <Sider>Sider</Sider>
-                <Content>Content</Content>
-            </Layout>
-            <Footer>Footer</Footer>
-        </Layout>
-
-        <Layout>
-            <Header>Header</Header>
-            <Layout>
-                <Content>Content</Content>
-                <Sider>Sider</Sider>
-            </Layout>
-            <Footer>Footer</Footer>
-        </Layout>
-
-        <Layout>
-            <Sider>Sider</Sider>
-            <Layout>
-                <Header>Header</Header>
-                <Content>Content</Content>
-                <Footer>Footer</Footer>
-            </Layout>
-        </Layout>
-    </>
-);
-
+    );
+};
 export default App;
