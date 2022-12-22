088619021373acc772957be21afb1d663f0cc8e6
a6a18 <39345520+a6a18@users.noreply.github.com>
2022-12-23 00:04:32 +0800

    Create Screen page
    建立一些基礎頁面，先有個route，內容之後再說

---
+++ src/screens/BlockChain/BlockChain.Screen.js
@@ -0,0 +1,5 @@
+function BlockChain() {
+    return <h1>BlockChain</h1>;
+}
+
+export default BlockChain;
---
+++ src/screens/Scrapying/Scrapying.Screen.js
@@ -0,0 +1,5 @@
+function Scrapying() {
+    return <h1>Scrapying</h1>;
+}
+
+export default Scrapying;
